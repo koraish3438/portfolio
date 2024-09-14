1a90cc56be2efd8e8efcb731a90a4a2c5060ed45
koraish3438 <koraishmdkajamall38@gmail.com>
2024-09-14 13:26:26 +0600

    about section start

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const menuicon =document.getElementById("menu-icon");
 const menu =document.getElementById("navbar");
 
-// use eventlistener (click listener)..click korle nav bar show krbe
+// show and hide menu
 menuicon.addEventListener("click", () => {
   if(menu.className === 'navbar'){
       menu.classList.remove('navbar');
